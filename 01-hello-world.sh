f73c3d365eae17c591df7d50697b4ccaa64b831c
charankumar07 <charankumar0999@gmail.com>
2024-01-28 13:45:13 +0530

    added 2nd script

---
+++ 01-hello-world.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "hello shell-script"
+echo "iam writing a code"
+echo "how are you guys"
---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh : hello suresh , good morning"
+echo "Suresh : hello ramesh , good morning, how are you ?"
+echo "Suresh : Yeah im fine ramesh and you?"
+echo "Ramesh : iam also good"
