f8f90f3b4d3fc1a45589386cc8b15cc7e7896077
charankumar07 <charankumar0999@gmail.com>
2024-02-02 15:02:58 +0530

    added 2nd script

--- 07-datatypes.sh
+++ 07-datatypes.sh
@@ -5,7 +5,7 @@ number2=$2
 
 sum=$($number1+$number2)
 
-echo "Total is :$sum"
+echo "Total is :$sum "
 echo "How many agruments are passed : $#"
 echo "All agrs passed: $@ "
 echo "Script name : $0"
