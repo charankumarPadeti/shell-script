d263a7a3589e053b2636b6c6376c03ee9e99cd84
charankumar07 <charankumar0999@gmail.com>
2024-02-03 08:01:08 +0530

    added 2nd script

---
+++ 12-logs.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+ID= (u-id)
+
+VALIDATE(){
+    if[ $1 -ne 0 ]
+    then
+        echo "..... failed"
+        exit 1
+
+
+    else
+        echo " ......SECUSS"
+}
+if[ ID -ne 0 ]
+then
+    echo "ERROR : Please go on root access"
+
+    exit 1
+
+else
+    echo "you are root user"
+
+install mysql  -y
+VALIDATE " installing mysql"
+install git -y
+VALIDATE " installing git"
