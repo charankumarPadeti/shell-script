bda6e0ce61838ae2a08625cf387c8962aa29c4a0
charankumar07 <charankumar0999@gmail.com>
2024-02-03 08:27:40 +0530

    added 2nd script

--- 12-logs.sh
+++ 12-logs.sh
@@ -2,6 +2,15 @@
 
 ID=$(id -u)
 
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "....FAILED"
+        exit 1
+    else
+        echo "....SUCESS"
+}
+
 if [ $ID -ne 0 ]
 then
     echo "ERROR : Please go on root access"
@@ -11,4 +20,9 @@ else
     echo "you are root user"
 
 fi
+
+install mysql -y
+VALIDATE $?
+install mygit -y
+VALIDATE $?
 
