787868c517793a5b98fb98912b97be55b9091d9e
charankumar07 <charankumar0999@gmail.com>
2024-02-03 22:44:09 +0530

    added 2nd script

---
+++ 13-install-packages.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+y="\e[33m"
+N="\e[0m"
+
+if[ $ID -ne 0]
+then
+    echo -e" ERROR : $R Please go with root acess $N"
+    exit 1
+else
+    echo -e " $R you are root user $N"
+fi
+
+echo "All agruments passed : $@"
