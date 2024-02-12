e0af8df9fdb73f7655cd1c3faf3b1954d3b5e2b4
charankumar07 <charankumar0999@gmail.com>
2024-02-12 13:45:38 +0530

    added 2nd script

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -7,12 +7,37 @@ G="\e[32m"
 y="\e[33m"
 N="\e[0m"
 
+TIMESTAMP= $(date +F%-%H-%M-%S)
+
+LOGFILE ="/tmp/$0-$TIMESTAMP.log"
+
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e " $2....$R FAILURE $N"
+    else
+        echo -e " $2...$G SUCCESS $N"
+}
 if [ $ID -ne 0 ]
 then
     echo -e " ERROR : $R Please go with root acess $N"
     exit 1
+
 else
     echo -e " $R you are root user $N"
 fi
 
-echo "Allll agruments passed : $@"
+# echo "Allll agruments passed : $@"
+
+for package in $@
+do 
+    yum list installed  $package &>> $LOGFILE
+    if [ $? -ne 0 ]
+    then
+        yum install $package -y &>> $LOGFILE
+        VALIDATE $? "installation of $packages "
+    else
+        echo -e "$package is already installed ...$Y SKIPPING $n "
+    fi
+done
