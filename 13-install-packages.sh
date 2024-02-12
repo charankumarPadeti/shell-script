9a85408a763cacfdfcaf2d8fd4fabc8d99b2bd1d
charankumar07 <charankumar0999@gmail.com>
2024-02-12 13:54:19 +0530

    added 2nd script

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -9,13 +9,14 @@ N="\e[0m"
 
 TIMESTAMP=$(date +F%-%H-%M-%S)
 
-LOGFILE ="/tmp/$0-$TIMESTAMP.log"
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
         echo -e " $2....$R FAILURE $N"
+        exit 1
     else
         echo -e " $2...$G SUCCESS $N"
 }
