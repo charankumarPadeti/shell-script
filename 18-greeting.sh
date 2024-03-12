013f39f61aa3508b3477d76d8ebc9d413008ab27
charankumar07 <charankumar0999@gmail.com>
2024-03-12 11:29:18 +0530

    roboshop shell scripts

--- 18-greeting.sh
+++ 18-greeting.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
-PERSON=$1
+NAME=""
+WISHES=""
 
-echo "hello $PERSON GOOD MORNING HOW ARE YOU"
+USAGE(){
+    echo "USAGE:: $(basename $0) -n <name> -w <wishes>"
+    echo "Options::"
+    echo "-n, specify the name (mandatory)"
+    echo " -w, specify the wishes. ex, Good morning"
+    echo "-h, Display the help and exit"
+}
+
+while getopts ":n:w:h" opt; do
+    case $opt in
+        n ) NAME="$OPTARG";;
+        w ) WISHES="$OPTARG";;
+        h |*) USAGE; exit;;
+    esac
+done
